67da7340838508a410545a08fa7390fcd7722d0f
viDaLost <vitaledanilov@gmail.com>
2026-04-19 13:23:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbw6z8q4Cw2JIaCCI63py0SxO-ugCYnhz5IQ5ljg9a7pdLBOHgNkBSUAruAomWPG9fSc2g/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbyfuRrUxGwacwKRyolVcXTPnENkGxayaSVXx-fDvCT70CYi2YmMyAfryAhNQxE7iwdn/exec'
 };
